4160c2be7ab06507abd0b7a057d1bedb34bdef74
trung <tranthanhtrung1983@yahoo.com>
2026-03-01 11:18:18 +0100

    updated appserver

--- demo-app/server.js
+++ demo-app/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(express.static('app/public'))
+app.use(express.static('demo-app/public'))
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' })
